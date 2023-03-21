68faed5518c30b967a939cbd8c7577ebb8559d07
zippiespick <zippiespick@gmail.com>
2023-03-21 16:21:51 -0400

    updated

---
+++ my-portfolio/public/src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import PortfolioContainer from "./my-portfolio/public/src/components/PortfolioContainer";
+
+const App = () => <PortfolioContainer />;
+
+export default App;
---
+++ my-portfolio/public/src/components/pages/NavTags.js
@@ -0,0 +1,48 @@
+import React from 'react';
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#works"
+          onClick={() => handlePageChange('Works')}
+
+          className={currentPage === 'Works' ? 'nav-link active' : 'nav-link'}
+        >
+          Works
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          //  TODO: Add a comment explaining what this logic is doing
+
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
